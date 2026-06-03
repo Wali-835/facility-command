35936a02c7b1ca6bd37bb5698426b4ea429c8108
Wali-835 <ahmedwali835@gmail.com>
2026-06-03 06:47:09 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import AssetPage from './AssetPage.jsx'
+
+const isAssetPage = new URLSearchParams(window.location.search).get("asset");
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {isAssetPage ? <AssetPage /> : <App />}
   </StrictMode>,
 )
